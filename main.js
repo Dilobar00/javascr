715ceb1bd55e946729885a8fb98b8b634dfe49b9
dilobar00 <dilobarmahmudjonova@gmail.com>
2024-02-15 07:29:53 +0300

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+const text = "jumavoy qalesan dostim ishlaring yaxshimi"
+let result = text.split(" ")
+  
+
+const firstChar = text.charAt().toUpperCase();
+const restChar = text.slice(1);
+const end = firstChar + restChar;
+
+console.log(end);
